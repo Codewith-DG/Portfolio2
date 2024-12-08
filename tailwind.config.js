c415db4455bbf4001efa0cd328ab4e3f06674e36
Dipak Ghige <dipakghige2512@gmail.com>
2024-12-09 01:17:03 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'regal-blue': '#d1e7fe',
+      },
+
+    },
+  },
+  plugins: [],
+}
+
